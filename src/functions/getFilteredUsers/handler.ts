676146db020b5120c2e53340177a0b07b9d5404a
iMelki <melki@post.bgu.ac.il>
2021-09-02 23:22:18 +0300

    removed lodash

--- src/functions/getFilteredUsers/handler.ts
+++ src/functions/getFilteredUsers/handler.ts
@@ -4,20 +4,46 @@ import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 
-import {filter} from 'lodash';
+// import {filter} from 'lodash';
 
 import schema from './schema';
+
+function isObjectMatchedToFilter<T> (obj: T, filter: T) : boolean {
+  let ans: boolean = true;
+  Object.keys(filter).forEach(key => {
+      if((typeof obj[key] === 'string' 
+        && typeof filter[key] === 'string' 
+        && obj[key] !== filter[key])
+        || (typeof obj[key] === 'number' 
+        && typeof filter[key] === 'number'  
+        && obj[key] !== filter[key]) 
+      ) {
+        ans = false;
+      }
+      else {
+        if (typeof obj[key] === 'object' && 
+            isObjectMatchedToFilter(obj[key], filter[key]) === false)
+            ans = false;
+      }
+    });
+  return ans;
+}
 
 const getFilteredUsers: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   let users: IUser[] = event.body.users;
   users = users.map((user, i) => {user.id = i; return user;});
-  let filterObj: IUserFilterObject = event.body.filter;
-  let filteredUsers: IUser[] = filter(users, filterObj as IUser);
+  const filterObj: IUserFilterObject = event.body.filter;
+  // const filteredUsers: IUser[] = filter(users, filterObj as IUser);
+  const filteredUsers: IUser[] = users.filter((user) => isObjectMatchedToFilter<IUser>(user, filterObj as IUser));
 
   return formatJSONResponse({
     filteredUsers: filteredUsers,
     // event,
   });
+
+  
+  
+
 }
 
 export const main = middyfy(getFilteredUsers);
